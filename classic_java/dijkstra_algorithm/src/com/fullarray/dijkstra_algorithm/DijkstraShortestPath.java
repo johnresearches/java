db2122439e1e277ceb71ad21411bb2bb9300a52e
johnresearches <76896708+johnresearches@users.noreply.github.com>
2021-01-06 23:02:17 -0500

    Update DijkstraShortestPath.java

--- classic_java/dijkstra_algorithm/src/com/fullarray/dijkstra_algorithm/DijkstraShortestPath.java
+++ classic_java/dijkstra_algorithm/src/com/fullarray/dijkstra_algorithm/DijkstraShortestPath.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
 
-
 public class DijkstraShortestPath{
 	
 	public void computeShortestPaths(Vertex sourceVertex) {
