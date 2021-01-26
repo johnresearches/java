a4210308803b6b34b7f4a551c7c353c09d9aba44
johnresearches <76896708+johnresearches@users.noreply.github.com>
2021-01-25 21:38:45 -0500

    added new

---
+++ competitive/hr/StringOuput.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class StringOuput {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                String xs = Integer.toString(x);
+                String xpadded="000".substring(xs.length()) + xs;
+                //Complete this line
+                int diff = 18 - s1.length();
+                System.out.println(s1 +""+String.format("%" + diff + "s", xpadded));
+                
+            }
+            System.out.println("================================");
+
+    }
+}
