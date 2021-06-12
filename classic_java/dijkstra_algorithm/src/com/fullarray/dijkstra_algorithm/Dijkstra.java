22fbaf12e3c538983c343a133b8bcd28e29ff619
johnresearches <76896708+johnresearches@users.noreply.github.com>
2021-06-11 21:46:14 -0400

    Update Dijkstra.java

--- classic_java/dijkstra_algorithm/src/com/fullarray/dijkstra_algorithm/Dijkstra.java
+++ classic_java/dijkstra_algorithm/src/com/fullarray/dijkstra_algorithm/Dijkstra.java
@@ -3,9 +3,13 @@ package com.fullarray.dijkstra_algorithm;
 public class Dijkstra {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
+		
 		Vertex vertexA = new Vertex("A");
+		
 		Vertex vertexB = new Vertex("B");
+		
 		Vertex vertexC = new Vertex("C");
 		Vertex vertexD = new Vertex("D");
 		Vertex vertexE = new Vertex("E");
