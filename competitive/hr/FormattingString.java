8bf990770514689603f59960c7923561a34b66ca
johnresearches <76896708+johnresearches@users.noreply.github.com>
2021-08-15 22:19:08 -0400

    Update FormattingString.java

--- competitive/hr/FormattingString.java
+++ competitive/hr/FormattingString.java
@@ -5,6 +5,7 @@ public class FormattingString {
         
             Scanner sc=new Scanner(System.in);
             System.out.println("================================");
+        
             for(int i=0;i<3;i++){
                 String s1=sc.next();
                 int x=sc.nextInt();
