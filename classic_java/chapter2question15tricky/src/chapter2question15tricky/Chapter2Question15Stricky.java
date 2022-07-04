89821ca8965522f8a0feebe60d357b5a0048b539
johnresearches <76896708+johnresearches@users.noreply.github.com>
2022-07-03 21:08:59 -0400

    Added tricky question functionally

---
+++ classic_java/chapter2question15tricky/src/chapter2question15tricky/Chapter2Question15Stricky.java
@@ -0,0 +1,18 @@
+package chapter2question15tricky;
+
+public class Chapter2Question15Stricky {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x =1, y = 15;
+		
+		while (x<10) {
+			y--;
+			x++;
+		}
+		
+		System.out.println(x+", "+y);
+		
+	}
+
+}
